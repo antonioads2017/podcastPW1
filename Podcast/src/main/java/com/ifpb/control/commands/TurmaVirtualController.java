cf41cfcaf74cae8c5c10f75fce72fd2eb3f8772a
MailsonD <mailssondennis@gmail.com>
2019-02-28 09:12:28 -0300

    Criacao de controller para turmas virtuais e seus devidos servicos

---
+++ Podcast/src/main/java/com/ifpb/control/commands/TurmaVirtualController.java
@@ -0,0 +1,75 @@
+package com.ifpb.control.commands;
+
+import com.ifpb.control.commands.Exceptions.CommandException;
+import com.ifpb.model.dao.impl.TurmaVirtualDaoImpl;
+import com.ifpb.model.dao.interfaces.TurmaVirtualDao;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Mailson Dennis
+ *
+ */
+public class TurmaVirtualController implements Command {
+
+    private TurmaVirtualDao turmaVirtualDao;
+
+    public TurmaVirtualController(){
+        turmaVirtualDao = new TurmaVirtualDaoImpl();
+    }
+
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws CommandException {
+        String acao = request.getParameter("acao");
+        switch(acao){
+            case "criar":
+                criarTurmaService(request,response);
+                break;
+            case "deletar":
+                deletarTurmaService(request,response);
+                break;
+            case "listar":
+                listarTurmaService(request,response);
+                break;
+            case "buscar":
+                buscarTurmaService(request,response);
+                break;
+            case "atualizar":
+                atualizarTurmaSerice(request,response);
+                break;
+            case "adicionarAluno":
+                adicionarAlunoService(request,response);
+                break;
+            case "adicionarPodcast":
+                adicioanarPodcastService(request,response);
+                break;
+        }
+    }
+
+
+    private void criarTurmaService(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void deletarTurmaService(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void listarTurmaService(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void buscarTurmaService(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void adicionarAlunoService(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void adicioanarPodcastService(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void atualizarTurmaSerice(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+
+}
