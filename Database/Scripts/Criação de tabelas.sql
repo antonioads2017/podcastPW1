91bd962189739480bdd2afc5bb5376ea3139dae7
MailsonD <mailssondennis@gmail.com>
2019-03-14 09:12:27 -0300

    Adição de restrições aos scripts de criação do banco

--- Database/Scripts/Criação de tabelas.sql
+++ Database/Scripts/Criação de tabelas.sql
@@ -6,11 +6,11 @@
 CREATE TABLE usuario(
 	email TEXT PRIMARY KEY,
 	senha TEXT NOT NULL,
-	admin BOOLEAN,
-	nome TEXT,
+	admin BOOLEAN NOT NULL,
+	nome TEXT NOT NULL,
 	foto VARCHAR(50),
-	nascimento DATE,
-    sexo VARCHAR(10),
+	nascimento DATE NOT NULL,
+    sexo VARCHAR(10) NOT NULL,
     telefone VARCHAR(15)
 );
 
@@ -27,25 +27,25 @@ CREATE TABLE professor(
 CREATE TABLE turma_virtual(
 	nome VARCHAR(30) PRIMARY KEY,
 	descricao TEXT,
-	professor_email TEXT,
+	professor_email TEXT NOT NULL,
 	FOREIGN KEY (professor) REFERENCES professor(email)
 );
 
 CREATE TABLE podcast(
 	audio VARCHAR(50) PRIMARY KEY,
-	titulo VARCHAR(30),
-	categoria TEXT,
+	titulo VARCHAR(30) NOT NULL,
+	categoria TEXT NOT NULL,
 	descricao TEXT,
-	criador TEXT,
-	nome_turma VARCHAR(30),
+	criador TEXT NOT NULL,
+	nome_turma VARCHAR(30) NOT NULL,
 	FOREIGN KEY (criador) REFERENCES usuario(email),
 	FOREIGN KEY (nome_turma) REFERENCES turma_virtual(nome)
 );
 
 CREATE TABLE avalia_podcast(
-	usuario TEXT,
-	podcast VARCHAR(50),
-	comentario TEXT,
+	usuario TEXT NOT NULL,
+	podcast VARCHAR(50) NOT NULL,
+	comentario TEXT NOT NULL,
 	PRIMARY KEY (usuario,podcast),
 	FOREIGN KEY (usuario) REFERENCES usuario(email),
 	FOREIGN KEY (podcast) REFERENCES podcast(audio)
